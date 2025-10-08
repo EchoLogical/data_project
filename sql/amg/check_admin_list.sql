f9425fe3fc99e8656a619609c2357ff9ec4a94c8
Faisal <faisal.amir@avrist.com>
2025-10-08 17:10:46 +0700

    update

--- sql/amg/check_admin_list.sql
+++ sql/amg/check_admin_list.sql
@@ -11,8 +11,56 @@ SELECT admin_login.id          as id,
 FROM admin_login
          LEFT OUTER JOIN broker_data ON admin_login.id = broker_data.adminId
 WHERE admin_login.ROLE NOT IN ('EBD', 'admin_ebd')
-ORDER BY admin_login.id DESC
-
-
+ORDER BY admin_login.id DESC;
 
 select distinct role from admin_login;
+
+WITH duplicate_check as (
+    SELECT email,
+         COUNT(*) AS duplicate_count
+    FROM admin_login
+    GROUP BY email
+    HAVING COUNT(*) > 1
+)
+select
+    al.id as admin_id,
+    ed.id as ebd_id,
+    hr.id as hr_id,
+    bd.id as broker_id,
+    al.createdAt as admin_created_at,
+    al.updatedAt as admin_updated_at,
+    al.email as admin_email,
+    al.role as admin_role,
+    al.isActive as admin_isActive,
+    ed.email as ebd_email,
+    tu.user_name as trole_user_name,
+    tm.name as role_permission
+from admin_login al
+left join ebd_data ed on al.id = ed.admin_id
+left join trole_user tu on al.email = tu.user_name
+left join trole_menu tm on tu.role_menu_id = tm.id
+left join hr_data hr on hr.adminId = al.id
+left join broker_data bd on bd.adminId = al.id
+where al.email in (
+    select email from duplicate_check
+)
+
+
+select * from ebd_data
+where member_id = 'dsnp124'
+
+select * from admin_login
+where id = 1081
+
+delete from admin_login where id in (
+    20439
+);
+
+delete from ebd_data
+where member_id = 'dsnp124';
+
+select * from admin_login
+where email = 'faisal.amir@avrist.com';
+
+
+
--- sql/ebd/check_ebd_compass.sql
+++ sql/ebd/check_ebd_compass.sql
@@ -1,20 +1,19 @@
 select * from COMPINAP_SRVAE
-where srvae = 'DSNP028';
+where srvae = 'DSNP124';
 
 delete from COMPINAP_SRVAE
-where srvae = 'DSNP028';
+where srvae = 'DSNP124';
 
 DECLARE
-    @ldapcd VARCHAR(10) = 'DSNP028';
+    @ldapcd VARCHAR(10) = 'DSNP124';
 select * from SRVAE_HIERARCHY
 where ldap_cd_member = @ldapcd or ldap_cd_div_head = @ldapcd or ldap_cd_dept_head = @ldapcd;
 
-
 delete from SRVAE_HIERARCHY
-where ldap_cd_dept_head = 'DSNP028';
+where ldap_cd_dept_head = 'DSNP124' OR ldap_cd_member = 'DSNP124';
 
 select * from COMPINAP_SRVAE_POLICY
-where srvae = 'DSNP028';
+where srvae = 'DSNP124';
 
 delete from COMPINAP_SRVAE_POLICY
-where srvae = 'DSNP028';
+where srvae = 'DSNP124';
